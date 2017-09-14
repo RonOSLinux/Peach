a388bc4d971219cd96926693e47ed2b27beefa2b
EasyDev <easy-dev@web.de>
2017-09-14 11:50:13 +0200

    Added a Timer to the Corners

--- src/main.cpp
+++ src/main.cpp
@@ -11,6 +11,7 @@ int main(int argc,char* argv[]) {
     bool end=false;
     SDL_DisplayMode dm;
     SDL_Event e;
+    Uint32 timer=SDL_GetTicks();
 
     resetAllModules();
 
@@ -41,39 +42,45 @@ int main(int argc,char* argv[]) {
     while(!end) {
       SDL_PollEvent(&e);
       SDL_GetGlobalMouseState(&mx,&my);
-
-      if(SDL_BUTTON(SDL_GetGlobalMouseState(&mx,&my))<=0) {
-        if(mx==0&&my==0) {
-          if(getModule("apps")!=1) {
-            t=std::system(("~/"+PEACH_FOLDER+"bin/apps &").c_str());
-            setModule("apps",1);
-          }
-        } else if(mx==dm.w-1&&my==0) {
-          if(getModule("software")!=1) {
-            t=std::system(("~/"+PEACH_FOLDER+"bin/software-explorer &").c_str());
-            setModule("software",1);
-          }
-        } else if(mx==0&&my==dm.h-1) {
-          if(getModule("restart")!=1&&getModule("shutdown")!=1) {
-            t=std::system(("~/"+PEACH_FOLDER+"bin/restart &").c_str());
-            setModule("restart",1);
-          }
-        } else if(mx==dm.w-1&&my==dm.h-1) {
-          if(getModule("shutdown")!=1&&getModule("restart")!=1) {
-            t=std::system(("~/"+PEACH_FOLDER+"bin/shutdown &").c_str());
-            setModule("shutdown",1);
-          }
-        } else if(mx>dm.w/2-368/2&&mx<(dm.w/2-368/2)+368&&my==dm.h-1) {
-          if(getModule("panel")!=2) {
-            setModule("panel",2);
-          }
-        } else if(mx>dm.w/2-368/2&&mx<(dm.w/2-368/2)+368&&my==0) {
-          if(getModule("panel")!=2) {
-            setModule("panel",2);
+      if(SDL_GetTicks()-timer>=650) {
+        if(SDL_BUTTON(SDL_GetGlobalMouseState(&mx,&my))<=0) {
+          if(mx==0&&my==0) {
+            if(getModule("apps")!=1) {
+              t=std::system(("~/"+PEACH_FOLDER+"bin/apps &").c_str());
+              setModule("apps",1);
+            }
+            timer=SDL_GetTicks();
+          } else if(mx==dm.w-1&&my==0) {
+            if(getModule("software")!=1) {
+              t=std::system(("~/"+PEACH_FOLDER+"bin/software-explorer &").c_str());
+              setModule("software",1);
+            }
+            timer=SDL_GetTicks();
+          } else if(mx==0&&my==dm.h-1) {
+            if(getModule("restart")!=1&&getModule("shutdown")!=1) {
+              t=std::system(("~/"+PEACH_FOLDER+"bin/restart &").c_str());
+              setModule("restart",1);
+            }
+            timer=SDL_GetTicks();
+          } else if(mx==dm.w-1&&my==dm.h-1) {
+            if(getModule("shutdown")!=1&&getModule("restart")!=1) {
+              t=std::system(("~/"+PEACH_FOLDER+"bin/shutdown &").c_str());
+              setModule("shutdown",1);
+            }
+            timer=SDL_GetTicks();
+          } else if(mx>dm.w/2-368/2&&mx<(dm.w/2-368/2)+368&&my==dm.h-1) {
+            if(getModule("panel")!=2) {
+              setModule("panel",2);
+            }
+            timer=SDL_GetTicks();
+          } else if(mx>dm.w/2-368/2&&mx<(dm.w/2-368/2)+368&&my==0) {
+            if(getModule("panel")!=2) {
+              setModule("panel",2);
+            }
+            timer=SDL_GetTicks();
           }
         }
       }
-
       if(getModule("action")==1) {
         t=std::system("poweroff &");
         setModule("action",0);
@@ -83,7 +90,6 @@ int main(int argc,char* argv[]) {
         setModule("action",0);
         t=std::system(("~/"+PEACH_FOLDER+"bin/desktop fade-out").c_str());
       }
-
       SDL_Delay(35);
     }
     SDL_Quit();
